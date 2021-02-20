02ddbbf910cfb90e4d8d666756906ef7956666dd
IsaacThoman <isaacthoman@outlook.com>
2021-02-20 10:36:30 -0500

    figured out the polygon stuff

--- neuralNet/Form1.vb
+++ neuralNet/Form1.vb
@@ -1,3 +1,43 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim surface As Graphics = CreateGraphics()
+        Dim Brush1 As Brush
+        Dim solidBrushGreen As SolidBrush = New SolidBrush(Color.DarkGreen)
+        Dim penGreen As Pen = New Pen(Color.DarkGreen)
+        Dim solidBrushGray As SolidBrush = New SolidBrush(Color.LightGray)
+        Dim solidBrushYellow As SolidBrush = New SolidBrush(Color.LightYellow)
+        Dim rects(8, 8) As Rectangle
+
+        Dim myGraphics As Graphics
+
+
+
+        Dim rects2 As Rectangle = New Rectangle(10, 10, 10, 10)
+        Dim point1 As PointF = New PointF(10, 10)
+        Dim point2 As PointF = New PointF(150, 150)
+        Dim point3 As PointF = New PointF(10, 150)
+        Dim point4 As PointF = New PointF(50, 60)
+        Dim curve = {point1, point2, point3}
+
+
+        surface.DrawLine(penGreen, point1, point2)
+        surface.FillPolygon(solidBrushGreen, curve)
+        'surface.FillRectangle(solidBrushGreen, rects2)
+
+
+        myGraphics = Graphics.FromHwnd(ActiveForm().Handle)
+
+
+
+
+
+    End Sub
+
+
 End Class
